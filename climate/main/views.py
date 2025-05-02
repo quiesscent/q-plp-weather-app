3206a6d3d8f3548c703a1bcc14b671c619cdac3e
quiesscent <ciphedc@gmail.com>
2025-05-02 17:08:39 +0300

    initial

---
+++ climate/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+import requests
+from django.conf import settings
+# Create your views here.
+
+def index(request):
+    api = settings.WEATHER_API_KEY
+    data = {}
+    if request.method == 'POST':
+        city = request.POST['city']
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=metric"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            result = response.json()
+    context = {
+    'data': result
+    }
+    return render(request, 'index.html', context)
