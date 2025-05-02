0ec1134fb60634a6e0f1b79f95c2c01ed22132dd
quiesscent <ciphedc@gmail.com>
2025-05-02 17:21:06 +0300

    final dummy

--- climate/main/views.py
+++ climate/main/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
 import requests
 from django.conf import settings
-# Create your views here.
+import json
 
 def index(request):
     api = settings.WEATHER_API_KEY
-    data = {}
+    result = {}
     if request.method == 'POST':
         city = request.POST['city']
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=metric"
         response = requests.get(url)
+        print(response.json())
+        result = response.json()
 
-        if response.status_code == 200:
-            result = response.json()
-    context = {
-    'data': result
-    }
-    return render(request, 'index.html', context)
+
+    return render(request, 'index.html', {"data": result })
